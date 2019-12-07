7455fef8dd03a885dfc03a4177972a6b41c99a7c
claneo <claneo@outlook.com>
2019-12-07 17:06:57 +0800

    add prettier config

---
+++ prettier.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "avoid",
+  rangeStart: 0,
+  rangeEnd: null,
+  proseWrap: "preserve"
+};
